1370ae2be38554cee510affe6c14f1752b0feeb0
Anderson Laventure <Laventurepsu1@gmail.com>
2026-03-18 23:29:18 -0400

    feat: Enhance employee API with validation middleware and improved filtering options

--- server/src/employee-directory-api.ts
+++ server/src/employee-directory-api.ts
@@ -54,6 +54,19 @@ let employees: Employee[] = [
   },
 ];
 
+const validationMiddleWare = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  const { name, department, role } = req.body;
+
+  if (!name || !department || !role)
+    return res.status(400).send("Bad request missing input");
+
+  next();
+};
+
 const app = express();
 
 app.use(cors());
@@ -62,16 +75,78 @@ app.use(express.json());
 // Your routes go here
 
 app.get("/api/employees", (req, res, next) => {
+  let filteredEmployees: Employee[] = employees;
   try {
-    const { department } = req.query;
-    if (!department) {
-      return res.status(200).json({ employees });
-    } else {
-      const filteredDeptEmployees = employees.filter(
-        (e) => e.department === department,
-      );
-      return res.status(200).json({ employees: filteredDeptEmployees });
+    const { department, isRemote } = req.query;
+
+    if (department) {
+      filteredEmployees = employees.filter((e) => e.department === department);
     }
+
+    if (isRemote === "true") {
+      filteredEmployees = filteredEmployees.filter((e) => e.isRemote);
+    }
+
+    if (isRemote === "false") {
+      filteredEmployees = filteredEmployees.filter((e) => !e.isRemote);
+    }
+
+    return res.status(200).send({ employees: filteredEmployees });
+  } catch (e) {
+    next(e);
+  }
+});
+
+app.get("/api/employees/summary", (req, res, next) => {
+  const employeesCount = employees.reduce(
+    (acc, val) => {
+      acc["total"] = (acc["total"] || 0) + 1;
+      acc["byDepartment"] = acc["byDepartment"] || {};
+      acc["byDepartment"][val.department] =
+        (acc["byDepartment"][val.department] || 0) + 1;
+      acc["remoteCount"] = (acc["remoteCount"] || 0) + (val.isRemote ? 1 : 0);
+      return acc;
+    },
+    {} as {
+      total: number;
+      byDepartment: { [key: string]: number };
+      remoteCount: number;
+    },
+  );
+
+  return res.status(200).json({ employees, employeesCount });
+});
+
+app.get("/api/employees/search", (req, res, next) => {
+  try {
+    const q = req.query.q as string;
+    if (!q) return res.status(400).send("Search word is missing");
+
+    const filteredEmployees = employees.filter((e) => {
+      return (
+        e.name.toLowerCase().includes(q.toLowerCase()) ||
+        e.department.toLowerCase().includes(q.toLowerCase()) ||
+        e.role.toLowerCase().includes(q.toLowerCase())
+      );
+    });
+
+    return res.status(200).json({ employees: filteredEmployees });
+  } catch (e) {
+    next(e);
+  }
+});
+
+app.post("/api/employees", validationMiddleWare, (req, res, next) => {
+  try {
+    const body = req.body;
+
+    const newEmployee = {
+      id: Math.random().toString(),
+      ...body,
+    };
+
+    employees = [...employees, newEmployee as Employee];
+    return res.status(201).json({ employee: newEmployee });
   } catch (e) {
     next(e);
   }
@@ -81,49 +156,31 @@ app.get("/api/employees/:id", (req, res, next) => {
   try {
     const id = req.params.id;
 
-    const employee = employees.find((e) => e.id === id);
-    if (!employee)
-      return res.status(404).send(`Employee with id:${id} not found`);
+    const emp = employees.find((e) => e.id === id);
+    if (!emp) return res.status(404).send("Employee not found");
 
-    return res.status(200).json({ employee });
+    return res.status(200).json({ employee: emp });
   } catch (e) {
     next(e);
   }
 });
 
-app.post("/api/employees", (req, res, next) => {
-  try {
-    const { name, department, role, isRemote } = req.body;
-
-    const newEmployee: Employee = {
-      name,
-      department,
-      role,
-      isRemote,
-      id: Math.random().toString(),
-    };
-
-    employees = [...employees, newEmployee];
-    return res.status(201).json({ employee: newEmployee });
-  } catch (e) {
-    next(e);
-  }
-});
-
-app.put("/api/employees/:id", (req, res, next) => {
+app.put("/api/employees/:id", validationMiddleWare, (req, res, next) => {
   try {
     const id = req.params.id;
     const body = req.body;
 
     const employeeToUpdate = employees.find((e) => e.id === id);
     if (!employeeToUpdate)
-      return res.status(404).send(`Employee with id ${id} not found`);
+      return res.status(404).send("Unable to find employee");
 
     employees = employees.map((e) => {
       return e.id === id ? { ...e, ...body } : e;
     });
 
-    res.status(200).json({ employee: employees.find((e) => e.id === id) });
+    return res
+      .status(200)
+      .json({ employees: employees.find((e) => e.id === id) });
   } catch (e) {
     next(e);
   }
@@ -133,8 +190,8 @@ app.delete("/api/employees/:id", (req, res, next) => {
   try {
     const id = req.params.id;
 
-    const itemToDelete = employees.find((e) => e.id === id);
-    if (!itemToDelete) return res.status(404).send("Unable to find");
+    const emp = employees.find((e) => e.id === id);
+    if (!emp) return res.status(404).send("Employee not found");
 
     employees = employees.filter((e) => e.id !== id);
 
@@ -144,40 +201,39 @@ app.delete("/api/employees/:id", (req, res, next) => {
   }
 });
 
-app.get("/api/external-users", async (req, res, next) => {
-  try {
-    const users = await fetch("https://jsonplaceholder.typicode.com/users");
-    if (!users.ok) return res.status(500).send("Unable to fetch Users");
-
-    const response = await users.json();
-    const EmailandNameUsers = response.map((user: any) => {
-      return {
-        name: user.name,
-        email: user.email,
-      };
-    });
-
-    return res.status(200).json({ users: EmailandNameUsers });
-  } catch (e) {
-    next(e);
-  }
-});
-
 app.patch("/api/employees/:id", (req, res, next) => {
   try {
     const id = req.params.id;
 
-    const user = employees.find((e) => e.id === id);
-    if (!user) return res.status(404).send("User not found for");
+    const emp = employees.find((e) => e.id === id);
+    if (!emp) return res.status(404).send("Unable to find user");
 
     employees = employees.map((e) => {
       return e.id === id ? { ...e, isRemote: !e.isRemote } : e;
     });
 
-
     return res
       .status(200)
       .json({ employee: employees.find((e) => e.id === id) });
+  } catch (e) {
+    next(e);
+  }
+});
+
+app.get("/api/external-users", async (req, res, next) => {
+  try {
+    const users = await fetch("https://jsonplaceholder.typicode.com/users");
+    if (!users.ok) throw new Error("Unable to fetch users");
+
+    let result = await users.json();
+    result = result.map((r) => {
+      return {
+        name: r.name,
+        email: r.email,
+      };
+    });
+
+    return res.status(200).json({ users: result });
   } catch (e) {
     next(e);
   }
